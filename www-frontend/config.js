f1b7260c68df25524b023e9ddb0bac32193bceff
CuvéeTRTL Node <leos.stehlik@gmail.com>
2019-10-31 17:50:38 +0000

    initial commit

---
+++ www-frontend/config.js
@@ -0,0 +1,27 @@
+var api = 'https://publicnode.ydns.eu/api2/'
+
+var api_blockexplorer = 'http://cuveedego.czech.cloud:6969'
+
+var poolHost = 'publicnode.ydns.eu:6969'
+
+var email = 'support@poolhost.com'
+
+var api_blockexplorer = ''
+
+var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
+
+var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
+
+var blockchainExplorer = 'https:///{symbol}/block/{id}'
+
+var transactionExplorer = 'https:///{symbol}/transaction/{id}'
+
+var themeCss = 'themes/default-theme.css'
+
+var networkStat = {
+  'bcn': [
+    ['bcn.mypool.online', 'http://bcn.mypool.online:8084'],
+    ['democats.org', 'http://pool.democats.org:7603']
+  ]
+}
+
