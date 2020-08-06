6481b304ea8c4f9f18990bdcd4ccf7362901964b
Tarmgas <48885901+Tarmgas@users.noreply.github.com>
2020-08-06 12:02:06 +0200

    Update config.js
    added blockexplorer link, and supportmail

--- www-frontend/config.js
+++ www-frontend/config.js
@@ -1,20 +1,18 @@
-var api = 'https://publicnode.ydns.eu/api2/'
+var api = 'http://dyngepeng.zapto.org:4010'
 
-var api_blockexplorer = 'http://cuveedego.czech.cloud:6969'
+var api_blockexplorer = 'http://dyngepeng.zapto.org/DYNGE/explorer'
 
-var poolHost = 'publicnode.ydns.eu:6969'
+var poolHost = 'dyngepeng.zapto.org'
 
-var email = 'support@poolhost.com'
-
-var api_blockexplorer = ''
+var email = 'support@dyngecoin.com'
 
 var cryptonatorWidget = ['{symbol}-BTC', '{symbol}-USD', '{symbol}-EUR']
 
 var easyminerDownload = 'https://github.com/zone117x/cryptonote-easy-miner/releases/'
 
-var blockchainExplorer = 'https:///{symbol}/block/{id}'
+var blockchainExplorer = 'http://dyngepeng.zapto.org/Dynge/explorer/?hash={id}#blockchain_block'
 
-var transactionExplorer = 'https:///{symbol}/transaction/{id}'
+var transactionExplorer = 'http://dyngepeng.zapto.org/Dynge/explorer/?hash={id}#blockchain_transaction'
 
 var themeCss = 'themes/default-theme.css'
 
